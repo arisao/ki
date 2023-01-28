9085d084603b196e4ed94a948f6065229c7b0e86
arisaohta <arisaohta@localhost>
2023-01-28 16:51:21 +0900

    Practice5no6-8 추

---
+++ lib/src/main/java/ki/Practice5/Practice5no8.java
@@ -0,0 +1,24 @@
+package ki.Practice5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice5no8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] num;
+		//配列を生成する
+		num = new int[10];
+		for (int i = 0; i < num.length; i++) {
+		System.out.println("数字を入力してください");
+		Scanner x = new Scanner(System.in);
+		int x1 = x.nextInt();
+		num[i] = x1;	
+		}
+		Arrays.sort(num);
+		
+		System.out.println(Arrays.toString(num));
+	}
+
+}
